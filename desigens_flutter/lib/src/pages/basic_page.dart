4475028fb823dda1ab3027c2675e21a661ef26db
Pierre Guillen <hi@cerbero.dev>
2019-09-18 17:20:39 -0500

    - Crear imagen

--- desigens_flutter/lib/src/pages/basic_page.dart
+++ desigens_flutter/lib/src/pages/basic_page.dart
@@ -3,15 +3,26 @@ import 'package:flutter/material.dart';
 class BasicPage extends StatelessWidget {
  // const BasicPage({Key key}) : super(key: key);
 
+  final estiloTitulo = TextStyle( fontSize: 20.0, fontWeight: FontWeight.bold );
+  final estiloSubTitulo = TextStyle( fontSize: 18.0);   
+
+  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Column(
         children: <Widget>[
-        Image(image: ),
-        Text('Cerbero Dev Page Basic'),
+        
+        _crearImagen(),
+
         ] 
       ),
+    );
+  }
+
+    Widget  _crearImagen() {
+    return Image(
+    image: NetworkImage('https://e.an.amtv.pe/actualidad-ica-municipalidad-restringio-venta-alcohol-semana-santa-n316155-624x352-455486.jpg'),
     );
   }
 }
